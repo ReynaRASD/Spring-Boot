c1a007d20ec3b6c65cec731d9dd4d5adb84b95c9
ReynaRASD <nyan12reyna@gmail.com>
2022-02-23 12:48:09 -0600

    get-post

--- src/main/java/com/generetion/demo/Saludo.java
+++ src/main/java/com/generetion/demo/Saludo.java
@@ -1,22 +1,35 @@
 package com.generetion.demo;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+// aqui le damos el poder a spring de crear los objetos
+// esto es inyeccion de dependencias
 public class Saludo {
-	
-	
-	
-@GetMapping("/")
+
+	@GetMapping("/")
 	public String saludo() {
-		return "Buenas tardes.";
+		return "Buenas tardesss.";
 	}
 
-@GetMapping("/saludo")
-	public String hello(@RequestParam(value="nombre", 
-	defaultValue = "Java") String nombre) {
-		return "Hello mom! I'm " + nombre;
+	@GetMapping("/saludo")
+	// datos por Get, no son datos sensibles
+	public String hello(@RequestParam(value = "nombre", defaultValue = "Java") String nombre,
+			@RequestParam(value = "apellidos") String apellidos) {
+		return "Hellooooo mom! I'm " + nombre + " " + apellidos;
+		// Como apellidos no tiene defaulValue, marca error, necesita a fuerzas el
+		// valor.
+		// En cambio nombre tiene default, no marca error si no le das un valor ya que
+		// tiene el default
+	}
+
+	@PostMapping("/login")
+	public String login(@RequestParam(value = "correo") String correo,
+			@RequestParam(value = "password") String password) {
+		return "Correo: " + correo + "\n" + "Password: " + password;
 	}
 
 }
